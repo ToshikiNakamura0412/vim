eb107ca7f1e4c566d47c38cc10d401b7acb822ac
Toshiki Nakamura <82020865+ToshikiNakamura0412@users.noreply.github.com>
2025-08-03 20:24:53 +0900

    Modify format (#17)

--- install.sh
+++ install.sh
@@ -1,70 +1,68 @@
 #!/bin/bash
 
-VIM_INSTALL_SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+VIM_INSTALL_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-function vim_get_script_dir() {
-  echo ${VIM_INSTALL_SCRIPT_DIR}
+vim_get_script_dir() {
+  echo "${VIM_INSTALL_SCRIPT_DIR}"
 }
 
-function vim_get_os_name() {
+vim_get_os_name() {
   if [[ "$(uname)" = "Linux" ]]; then
     source /etc/os-release
-    echo ${ID}
+    echo "${ID}"
   elif [[ "$(uname)" = "Darwin" ]]; then
-    echo ${ID}
+    echo "mac"
   else
-    echo "This OS is not supported."
+    echo "[ERROR] This OS is not supported."
     exit 1
   fi
 }
 
-function vim_install_prerequisites() {
-  local os_name=$(vim_get_os_name)
+vim_install_prerequisites() {
+  local os_name="$(vim_get_os_name)"
 
   echo ""
-  echo "install prerequisites..."
-  if [[ ${os_name} = "ubuntu" ]]; then
-    sudo apt-get update && sudo apt-get install -y --no-install-recommends \
-      curl \
-      vim-gtk
-  elif [[ ${os_name} = "debian" ]]; then
+  echo "[INFO] Installing prerequisites..."
+  if [[ "${os_name}" = "ubuntu" || "${os_name}" = "debian" ]]; then
     sudo apt-get update && sudo apt-get install -y --no-install-recommends \
       curl \
       vim-gtk3
-  elif [[ ${os_name} = "alpine" ]]; then
+  elif [[ "${os_name}" = "alpine" ]]; then
     sudo apk update && sudo apk add --no-cache \
       curl \
       vim
-  elif [[ ${os_name} = "fedora" ]]; then
+  elif [[ "${os_name}" = "fedora" ]]; then
     sudo dnf check-update || true && sudo dnf install -y --setopt=install_weak_deps=False \
       curl \
       vim
-  elif [[ ${os_name} = "centos" ]]; then
+  elif [[ "${os_name}" = "centos" ]]; then
     sudo dnf check-update || true && sudo dnf install -y --setopt=install_weak_deps=False \
       curl-minimal \
       vim
-  elif [[ ${os_name} = "opensuse-leap" ]]; then
+  elif [[ "${os_name}" = "opensuse-leap" ]]; then
     sudo zypper refresh && sudo zypper install -y --no-recommends \
       curl \
       vim
-  elif [[ ${os_name} = "arch" ]]; then
+  elif [[ "${os_name}" = "arch" ]]; then
     sudo pacman -Sy --noconfirm \
       curl \
       vim
-  elif [[ ${os_name} = "mac" ]]; then
+  elif [[ "${os_name}" = "mac" ]]; then
     brew install \
       vim
+  else
+    echo "[ERROR] Unsupported OS: ${os_name}"
+    exit 1
   fi
-  echo ">>> Done"
+  echo "[INFO] >>> Done"
   echo ""
 }
 
-function vim_setup() {
-  local script_dir=$(vim_get_script_dir)
-  echo ${script_dir}
+vim_setup() {
+  local script_dir="$(vim_get_script_dir)"
 
   echo ""
-  echo "setting Vim... "
+  echo "[INFO] Setting Vim... "
   if [[ -d ~/.vim/plugged ]]; then
     rm -rf ~/.vim/plugged
   fi
@@ -74,14 +72,14 @@ function vim_setup() {
   if [[ ! -d ~/.vim/undo ]]; then
     mkdir -pv ~/.vim/undo
   fi
-  ln -sfv ${script_dir}/vimrc ~/.vimrc
-  ln -sfv ${script_dir}/basic-settings.vim ~/.vim/basic-settings.vim
-  ln -sfv ${script_dir}/keymap.vim ~/.vim/keymap.vim
-  echo ">>> Done"
+  ln -sfv "${script_dir}/vimrc" ~/.vimrc
+  ln -sfv "${script_dir}/basic-settings.vim" ~/.vim/basic-settings.vim
+  ln -sfv "${script_dir}/keymap.vim" ~/.vim/keymap.vim
+  echo "[INFO] >>> Done"
   echo ""
 }
 
-function vim_main() {
+vim_main() {
   vim_install_prerequisites
   vim_setup
 }
